8bc6f6b128d9fc7d3da29b40633aea3adc5758a2
ZERO-DAWN-X <10898499@students.plymouth.ac.uk>
2023-11-08 16:47:26 +0530

    init

---
+++ app/Login/page.tsx
@@ -0,0 +1,237 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="w-[1440px] h-[1024px] relative bg-slate-950 rounded-[20px]">
+  <div className="w-[314px] h-[390px] left-[298px] top-[-50px] absolute bg-pink-500 rounded-full blur-[400.77px]" />
+  <div className="w-[279.64px] h-[242.40px] left-[1303px] top-[332.95px] absolute origin-top-left rotate-[-3.06deg] bg-fuchsia-600 bg-opacity-30 rounded-full blur-[328.92px]" />
+  <div className="w-[223px] h-[330px] left-[1282px] top-[847px] absolute bg-pink-500 rounded-full blur-[400.77px]" />
+  <div className="w-[1440px] h-[62px] left-0 top-0 absolute">
+    <div className="w-[1440px] h-[62px] left-0 top-0 absolute bg-white rounded-tl-[20px] rounded-tr-[20px]" />
+    <div className="w-[1380px] h-[19px] left-[27px] top-[27px] absolute justify-between items-center inline-flex">
+      <div className="justify-start items-center gap-[37.88px] flex">
+        <div className="justify-start items-start gap-[12.63px] flex">
+          <div className="w-[17.68px] h-[17.68px] bg-red-400 rounded-2xl border border-red-500" />
+          <div className="w-[17.68px] h-[17.68px] bg-amber-300 rounded-2xl border border-orange-400" />
+          <div className="w-[17.68px] h-[17.68px] bg-green-400 rounded-2xl border border-green-600" />
+        </div>
+        <div className="justify-start items-start gap-[41.67px] flex">
+          <div className="w-[27.78px] h-[20.20px] relative">
+            <div className="w-[27.78px] h-[20.20px] left-0 top-0 absolute bg-white rounded-sm border-2 border-neutral-500" />
+            <div className="w-[2.53px] h-[6.06px] left-[3.16px] top-[4.55px] absolute flex-col justify-start items-start gap-[3.03px] inline-flex" />
+          </div>
+          <div className="justify-start items-start gap-[41.67px] flex" />
+        </div>
+      </div>
+      <div className="w-[847px] h-[34px] pl-[12.63px] pr-[286.62px] py-[11.36px] bg-neutral-200 rounded-md flex-col justify-start items-start gap-[12.63px] inline-flex">
+        <div className="w-[782.84px] h-[17.68px] justify-between items-center inline-flex">
+          <div className="w-[17.68px] h-[17.68px] relative">
+            <div className="w-[17.68px] h-[17.68px] left-0 top-0 absolute bg-zinc-100 rounded-[8.84px] border border-neutral-500" />
+            <div className="w-[8.37px] h-[8.37px] left-[4.65px] top-[4.65px] absolute">
+            </div>
+          </div>
+          <div className="h-[16.41px] justify-start items-center gap-[11.36px] flex">
+            <div className="w-[10.10px] h-[15.15px] relative">
+              <div className="w-[10.10px] h-[8.98px] left-[-0px] top-[6.17px] absolute bg-neutral-400 rounded-sm" />
+              <div className="w-[7.86px] h-[12.35px] left-[1.12px] top-0 absolute rounded-[5.05px] border border-neutral-400" />
+            </div>
+            <div className="w-[277.78px] h-[16.41px]"><span className="text-neutral-600 text-[18.94px] font-medium font-['Inter'] leading-[80.71px]">zerogames</span><span className="text-neutral-600 text-[18.94px] font-medium font-['Inter'] leading-[80.71px]">.com</span><span className="text-neutral-600 text-[18.94px] font-medium font-['Inter'] leading-[80.71px]">/heropage</span></div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[185px] h-4 justify-between items-center flex">
+        <div className="w-6 h-6 relative">
+          <div className="w-6 h-6 left-0 top-0 absolute bg-white rounded-xl border-2 border-neutral-500" />
+        </div>
+        <div className="w-[18.94px] h-[26.52px] relative">
+          <div className="w-[18.94px] h-[17.36px] left-[-0px] top-[9.15px] absolute bg-white rounded-sm border-2 border-neutral-500" />
+        </div>
+        <div className="w-[18.94px] h-[18.94px] relative">
+        </div>
+        <div className="w-[26.22px] h-[25.25px] relative">
+          <div className="w-[19.43px] h-[19.43px] left-0 top-0 absolute bg-white rounded-sm border-2 border-neutral-500" />
+          <div className="w-[19.43px] h-[19.43px] left-[6.80px] top-[5.83px] absolute bg-white rounded-sm border-2 border-neutral-500" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="w-3.5 h-3.5 left-[709px] top-[514px] absolute bg-white rounded-2xl border border-white" />
+  <div className="w-3.5 h-3.5 left-[732px] top-[514px] absolute bg-white rounded-2xl border border-white" />
+  <div className="w-3.5 h-3.5 left-[755px] top-[514px] absolute bg-white rounded-2xl border border-white" />
+  <div className="w-3.5 h-3.5 left-[778px] top-[514px] absolute bg-white rounded-2xl border border-white" />
+  <div className="w-3.5 h-3.5 left-[801px] top-[514px] absolute bg-white rounded-2xl border border-white" />
+  <div className="w-[11px] h-[11px] left-[711px] top-[516px] absolute bg-black rounded-2xl border border-white" />
+  <div className="w-[11px] h-[11px] left-[734px] top-[516px] absolute bg-black rounded-2xl border border-white" />
+  <div className="w-3 h-[11px] left-[756px] top-[516px] absolute bg-black rounded-2xl border border-white" />
+  <div className="w-3 h-[11px] left-[802px] top-[516px] absolute bg-black rounded-2xl border border-white" />
+  <img className="w-[1237px] h-[385px] left-[143px] top-[103px] absolute rounded-[20px]" src="https://via.placeholder.com/1237x385" />
+  <img className="w-[250px] h-[120px] left-[146px] top-[580px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <div className="w-[41px] h-8 left-[146px] top-[531px] absolute text-white text-opacity-90 text-[18.94px] font-medium font-['Inter'] leading-[80.71px]">New</div>
+  <div className="w-[200px] h-[25px] left-[146px] top-[708px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">CyberPunk 2077</div>
+  <div className="w-[22px] h-[22px] left-[374px] top-[709px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[17px] h-[17px] left-[146px] top-[738px] absolute" />
+  <div className="w-[17px] h-[17px] left-[203px] top-[738px] absolute">
+  </div>
+  <div className="w-[62px] h-[21px] left-[326px] top-[735px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[21px] h-[21px] left-[230px] top-[736px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <div className="w-6 h-6 left-[675px] top-[507px] absolute" />
+  <img className="w-[250px] h-[120px] left-[473px] top-[579px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <img className="w-[250px] h-[120px] left-[804px] top-[579px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <img className="w-[250px] h-[120px] left-[1130px] top-[579px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <div className="w-[200px] h-[25px] left-[473px] top-[707px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">Spider Man - Remasted</div>
+  <div className="w-[200px] h-[25px] left-[804px] top-[707px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">Battlefield 1</div>
+  <div className="w-[200px] h-[25px] left-[1130px] top-[707px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">NFS Unbound</div>
+  <div className="w-[22px] h-[22px] left-[701px] top-[708px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[22px] h-[22px] left-[1032px] top-[708px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[22px] h-[22px] left-[1362px] top-[709px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[17px] h-[17px] left-[473px] top-[737px] absolute" />
+  <div className="w-[17px] h-[17px] left-[804px] top-[737px] absolute" />
+  <div className="w-[17px] h-[17px] left-[1130px] top-[737px] absolute" />
+  <div className="w-[17px] h-[17px] left-[530px] top-[737px] absolute">
+  </div>
+  <div className="w-[17px] h-[17px] left-[1187px] top-[737px] absolute">
+  </div>
+  <div className="w-[62px] h-[21px] left-[653px] top-[734px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[62px] h-[21px] left-[985px] top-[734px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[62px] h-[21px] left-[1315px] top-[735px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[21px] h-[21px] left-[557px] top-[735px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <div className="w-[21px] h-[21px] left-[861px] top-[735px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <div className="w-[21px] h-[21px] left-[1214px] top-[735px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <img className="w-[250px] h-[120px] left-[146px] top-[790px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <div className="w-[200px] h-[25px] left-[146px] top-[918px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">Injustice 2</div>
+  <div className="w-[22px] h-[22px] left-[374px] top-[919px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[17px] h-[17px] left-[146px] top-[948px] absolute" />
+  <div className="w-[17px] h-[17px] left-[203px] top-[948px] absolute">
+  </div>
+  <div className="w-[62px] h-[21px] left-[326px] top-[945px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <img className="w-[250px] h-[120px] left-[473px] top-[789px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <img className="w-[250px] h-[120px] left-[804px] top-[789px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <img className="w-[250px] h-[120px] left-[1130px] top-[789px] absolute rounded-[15px]" src="https://via.placeholder.com/250x120" />
+  <div className="w-[200px] h-[25px] left-[473px] top-[917px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">Battlefield V</div>
+  <div className="w-[200px] h-[25px] left-[804px] top-[917px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">GTA 5</div>
+  <div className="w-[200px] h-[25px] left-[1130px] top-[917px] absolute text-neutral-200 text-base font-bold font-['Poppins'] leading-[80.71px]">NFS Plyback</div>
+  <div className="w-[22px] h-[22px] left-[701px] top-[918px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[22px] h-[22px] left-[1032px] top-[918px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[22px] h-[22px] left-[1359px] top-[918px] absolute justify-center items-center inline-flex">
+    <div className="w-[22px] h-[22px] relative">
+    </div>
+  </div>
+  <div className="w-[17px] h-[17px] left-[473px] top-[947px] absolute" />
+  <div className="w-[17px] h-[17px] left-[804px] top-[947px] absolute" />
+  <div className="w-[17px] h-[17px] left-[1130px] top-[947px] absolute" />
+  <div className="w-[17px] h-[17px] left-[530px] top-[947px] absolute">
+  </div>
+  <div className="w-[17px] h-[17px] left-[861px] top-[947px] absolute">
+  </div>
+  <div className="w-[17px] h-[17px] left-[1187px] top-[947px] absolute">
+  </div>
+  <div className="w-[62px] h-[21px] left-[655px] top-[945px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[62px] h-[21px] left-[986px] top-[944px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[62px] h-[21px] left-[1315px] top-[944px] absolute text-white text-base font-bold font-['Poppins'] leading-[80.71px]">View</div>
+  <div className="w-[21px] h-[21px] left-[557px] top-[945px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <div className="w-[21px] h-[21px] left-[888px] top-[945px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <div className="w-[21px] h-[21px] left-[1214px] top-[945px] absolute">
+    <div className="w-[21px] h-[21px] left-0 top-0 absolute">
+    </div>
+  </div>
+  <div className="w-[92px] h-[960px] left-0 top-[63px] absolute bg-slate-950 bg-opacity-70 rounded-bl-[20px] border border-zinc-600 flex-col justify-start items-start inline-flex">
+    <div className="self-stretch p-6 bg-gray-800 rounded-tl-2xl rounded-tr-2xl justify-between items-center inline-flex" />
+    <div className="w-[23px] h-[22px] p-1.5 bg-gray-900 rounded-lg border border-zinc-600 justify-start items-start gap-2 inline-flex" />
+    <div className="self-stretch h-px bg-zinc-600" />
+    <div className="self-stretch h-[277.13px] pt-6 flex-col justify-start items-center gap-2 flex">
+      <div className="text-white text-xs font-medium font-['Poppins'] uppercase leading-3 tracking-wide">Main</div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-2.5 inline-flex">
+        <div className="w-5 h-5 justify-center items-center flex">
+          <div className="w-6 h-6 relative flex-col justify-start items-start flex" />
+        </div>
+      </div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex" />
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex" />
+      <div className="px-3 py-2.5 bg-gray-800 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-[22px] h-[22px] relative" />
+      </div>
+      <div className="px-3 py-2.5 bg-gray-800 rounded shadow justify-start items-start gap-2 inline-flex">
+        <div className="text-white text-sm font-normal font-['Poppins'] leading-tight">Dll Files</div>
+      </div>
+      <div className="w-6 h-6 relative">
+        <div className="w-[14.40px] h-[15.31px] left-[5.37px] top-[4.39px] absolute">
+        </div>
+      </div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-5 h-5 relative" />
+      </div>
+    </div>
+    <div className="self-stretch grow shrink basis-0 pt-6 flex-col justify-start items-center gap-2 flex">
+      <div className="text-white text-xs font-medium font-['Poppins'] uppercase leading-3 tracking-wide">Buy</div>
+      <div className="h-[72px] px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-[22px] h-[22px] relative" />
+      </div>
+      <div className="text-white text-xs font-medium font-['Poppins'] uppercase leading-3 tracking-wide">Settings</div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-[22px] h-[22px] relative" />
+      </div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-5 h-5 relative" />
+      </div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-5 h-5 relative" />
+      </div>
+      <div className="px-3 py-2.5 rounded-lg justify-start items-center gap-3 inline-flex">
+        <div className="w-5 h-5 relative" />
+      </div>
+    </div>
+    <div className="self-stretch h-px bg-zinc-600" />
+    <div className="self-stretch h-[88px] px-6 py-[26px] bg-gray-800 rounded-bl-2xl rounded-br-2xl flex-col justify-start items-start gap-2 flex">
+      <div className="w-[52px] h-9 relative">
+        <div className="w-[52px] h-8 left-0 top-[2px] absolute bg-gray-900 rounded-2xl" />
+        <div className="w-7 h-7 left-[22px] top-[4px] absolute bg-neutral-400 rounded-full shadow" />
+      </div>
+    </div>
+  </div>
+  <div className="w-[138px] h-[103px] left-[683px] top-[563px] absolute bg-cyan-400 bg-opacity-90 rounded-full blur-[239.28px]" />
+</div>
+    </div>
+  )
+}
+
+export default page
---
+++ components/sidebar.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { faCaretDown, faCartArrowDown, faCartFlatbed, faEnvelope, faEnvelopeOpen, faFile, faGamepad, faGear, faHome, faInfoCircle, faPuzzlePiece, faSave } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const sidebar = () => {
+  return (
+    <div>
+        <div class="primary-nav">
+        <div class="custom-logo"></div>
+            
+            <nav role="navigation" class="menu">
+
+                <a href="#" class="logotype"><strong class="zero">ZERO</strong> Infinite</a>
+
+
+                <div class="overflow-container">
+
+                    <ul class="menu-dropdown">
+
+                        <li class="sidebartext">MENU</li>
+
+                        <li><a href="#">Home</a><span class="icon"><i><FontAwesomeIcon icon={faHome} /></i></span></li>
+                        <li class="menu-hasdropdown">
+                            <a href="#">Games</a><span class="icon"><i><FontAwesomeIcon icon={faGamepad} /></i></span>
+
+                            <label title="toggle menu" for="settings">
+                                <span class="downarrow"><i><FontAwesomeIcon icon={faCaretDown} />&nbsp;</i></span>
+                            </label>
+
+                            <input type="checkbox" class="sub-menu-checkbox" id="settings" />
+
+                            <ul class="sub-menu-dropdown">
+                                <div class="sub-class">
+                                    <li><a href="" >Action</a></li>
+                                    <li><a href="" >Adventures</a></li>
+                                    <li><a href="" >Racing</a></li>
+                                    <li><a href="" >Arcade</a></li>
+                                    <li><a href="" >Mind</a></li>
+                                    <li><a href="" >RPG</a></li>
+                                    <li><a href="" >Simulator</a></li>
+                                </div>
+                            </ul>
+                        </li>
+
+                        <li><a href="#">Softwares</a><span class="icon"><i><FontAwesomeIcon icon={faPuzzlePiece} /></i></span></li>
+
+                        <li><a href="#">DLL Files</a><span class="icon"><i><FontAwesomeIcon icon={faFile} /></i></span></li>
+            
+                        <li><a href="#">Save Game Files</a><span class="icon"><i><FontAwesomeIcon icon={faSave} /></i></span></li>
+            
+                        <li class="sidebartext2">BUY </li>
+            
+                        <li><a href="#">Cart</a><span class="icon"><i><FontAwesomeIcon icon={faCartArrowDown} /></i></span></li>
+
+                        <li><a href="#">Cart</a><span class="icon"><i><FontAwesomeIcon icon={faCartFlatbed} /></i></span></li>
+
+                        <li class="sidebartext3">SETTINGS</li>
+            
+                        <li><a href="#">Info</a><span class="icon"><i><FontAwesomeIcon icon={faInfoCircle} /></i></span></li>
+            
+                        <li><a href="#">Settings</a><span class="icon"><i><FontAwesomeIcon icon={faGear} /></i></span></li>
+
+                    </ul>
+
+                </div>
+
+            </nav>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default sidebar
